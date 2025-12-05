b6bd3bf8d1fbb03e9c9f378d8df13557c9e9eaea
ManishKumarGB <manishgb2001@gmail.com>
2025-12-05 18:22:49 +0530

    second commit

--- Frontend/src/components/Table.jsx
+++ Frontend/src/components/Table.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import 
 
 const Table = () => {
   return (
     <>
     <div className='Table'>
         <h1>Students Table</h1>
-        <table>
+        <table className='line'>
             <thead>
                 <tr>
                     <th>Name</th>
-                    <th>age</th>
+                    <th>Age</th>
                     <th>Action</th>
                 </tr>
             </thead>
@@ -25,7 +26,7 @@ const Table = () => {
                     </td>
                 </tr>
                 <tr>
-                    <td>arvind</td>
+                    <td>Arvind</td>
                     <td>25</td>
                     <td><div>
                             <button>Edit</button>
@@ -34,7 +35,7 @@ const Table = () => {
                     </td>
                 </tr>
                 <tr>
-                    <td>shahsank</td>
+                    <td>Shashank</td>
                     <td>23</td>
                     <td><div>
                             <button>Edit</button>
@@ -43,7 +44,7 @@ const Table = () => {
                     </td>
                 </tr>
                 <tr>
-                    <td>darshan</td>
+                    <td>Darshan</td>
                     <td>25</td>
                     <td><div>
                             <button>Edit</button>
